6b6a370453785f189a5a554e3921b3e870cf5079
rijuthm@gmail.com <rijuthm@gmail.com>
2021-10-14 10:01:57 +0530

    added routes to app

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 import express from 'express';
-
 import loaders from './loaders/index.js';
+import * as routes from './routes/index.js';
 
 async function startServer() {
   const app = express();
 
   await loaders(app);
-
+  routes.init(app);
   app.listen(1000, (err) => {
     if (err) {
       console.log(err);
