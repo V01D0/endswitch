67337b0e74320324300d63a09578537b59513a08
rijuthm@gmail.com <rijuthm@gmail.com>
2021-10-14 15:44:09 +0530

    configured workers

--- jobs/index.js
+++ jobs/index.js
@@ -1,12 +1,20 @@
 import ms from 'ms';
 import Bree from 'bree';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const bree = new Bree({
   jobs: [
     'verify',
     {
-      name: 'verify',
-      interval: 'every 14 days',
+      name: 'verify-main',
+      interval: 'every five days',
+      path: path.join(__dirname, './verify.js'),
     },
   ],
 });
+
+export default bree;
