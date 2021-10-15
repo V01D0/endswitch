1d24908171d0894f363b2b8a18b2489174268918
rijuthm@gmail.com <rijuthm@gmail.com>
2021-10-15 16:38:58 +0530

    refactored mailer service

--- services/mailer.js
+++ services/mailer.js
@@ -1,20 +1,28 @@
 import sgMail from '@sendgrid/mail';
 import config from '../config/index.js';
+import { createRequire } from 'module';
 
 sgMail.setApiKey(config.sendgrid.apiKey);
 
 export default class MailerService {
   constructor() {}
 
-  async endSwitch(data) {
-    (data['from'] = config.sendgrid.email),
-      sgMail
-        .send(data)
-        .then((response) => {
-          console.log(`${data['to']} : ${response[0].statusCode}`);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+  async endSwitch() {
+    const require = createRequire(import.meta.url);
+    const data = require('../data/template.json');
+    for (const [key, value] of Object.entries(data['users'])) {
+      this.sendMail(value);
+    }
+  }
+  async sendMail(data) {
+    data['from'] = config.sendgrid.email;
+    sgMail
+      .send(data)
+      .then((response) => {
+        console.log(`${data['to']} : ${response[0].statusCode}`);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 }
