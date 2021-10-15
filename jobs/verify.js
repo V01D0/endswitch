67dfb486428e5e43857e1c622066675afee99606
rijuthm@gmail.com <rijuthm@gmail.com>
2021-10-15 16:28:09 +0530

    finalized verification job

--- jobs/verify.js
+++ jobs/verify.js
@@ -1,12 +1,14 @@
 import VerifyService from '../services/verify.js';
-import bree from './index.js';
+import MailerService from '../services/mailer.js';
 
+const mailerInstance = new MailerService();
 const verifyInstance = new VerifyService();
 try {
   if (verifyInstance.verifySwitch()) {
+    // await mailerInstance.endSwitch();
+    console.log('act');
   } else {
-    verifyInstance.initiateSwitch();
-    await bree.start('mailer');
+    await verifyInstance.initiateSwitch();
   }
 } catch (e) {
   console.log(e);
