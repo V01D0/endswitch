c2077e1f7f1c280c5a8b653c2c6ee3c643c76af6
Rijuth Menon <rijuthm@gmail.com>
2021-10-16 13:10:56 +0530

    added method to send verification mail

--- services/mailer.js
+++ services/mailer.js
@@ -10,10 +10,27 @@ export default class MailerService {
   async endSwitch() {
     const require = createRequire(import.meta.url);
     const data = require('../data/template.json');
-    for (const [key, value] of Object.entries(data['users'])) {
-      this.sendMail(value);
+    try {
+      for (const [key, value] of Object.entries(data['users'])) {
+        await this.sendMail(value);
+      }
+      return true;
+    } catch (e) {
+      console.log(e);
+      return false;
     }
   }
+
+  async sendVerificationMail(token) {
+    let data = {
+      to: config.verificationEmail,
+      subject: 'Please verify existence',
+      text: 'Verify your existence',
+      html: `Click <a href="${config.domain}/verify?token=${token}">here</a> to verify!`,
+    };
+    await this.sendMail(data);
+  }
+
   async sendMail(data) {
     data['from'] = config.sendgrid.email;
     sgMail
