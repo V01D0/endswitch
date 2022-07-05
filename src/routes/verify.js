6845dbbeb72fa5614c407b2dabc23ad58f05359d
V01D0 <38071738+V01D0@users.noreply.github.com>
2022-07-05 11:47:26 +0530

    verification route handler

--- src/routes/verify.js
+++ src/routes/verify.js
@@ -1,6 +1,32 @@
-module.exports = function (app) {
+const verify = require("../services/verify");
+
+module.exports = function (app, parser) {
   app.get("/verify/:jwt", (req, res) => {
-    console.log(req.params.jwt);
-    res.redirect("/admin");
+    // console.log(req.params.jwt);
+    const valid = verify.verifyToken(req.params.jwt);
+    if (valid.value === true) {
+      const id = valid.decoded.data;
+      console.log(id);
+      res.render("verify");
+    } else {
+      res.send("ERROR!");
+    }
+  });
+
+  app.post("/verify/:jwt", parser, (req, res) => {
+    const valid = verify.verifyToken(req.params.jwt);
+    if (valid.value === true) {
+      const id = valid.decoded.data;
+      const pwd = req.body.pwd;
+      let verified = false;
+      if (id === pwd) {
+        verified = true;
+      }
+      res.render("verify", {
+        verified: verified,
+        msg: "CANNOT VERIFY!",
+      });
+      console.log("id");
+    }
   });
 };
